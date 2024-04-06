1dbf15fc156cd6ecabd77015bb363c9935a3f2fd
harsha <harsha200124@gmail.com>
2024-04-06 11:58:37 +0530

    updated mongo

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,10 @@ const routes = require('./index');
 const connectionString = "mongodb://mrigank94:GqtjZDKarka3wHps@todo-shard-00-02.iwjw6.mongodb.net:27017,todo-shard-00-00.iwjw6.mongodb.net:27017,todo-shard-00-01.iwjw6.mongodb.net:27017/me?authSource=admin&replicaSet=Todo-shard-0&ssl=true";
 
 // MongoDB connection options
-const mongoOptions = {
-  useNewUrlParser: true, // Use new URL parser
-  useUnifiedTopology: true, // Use new Server Discover and Monitoring engine
-};
+
 
 mongoose
-  .connect(connectionString, mongoOptions)
+  .connect(connectionString)
   .then(() => console.log("Connected to db successfully"))
   .catch((ex) => console.log(ex));
 
